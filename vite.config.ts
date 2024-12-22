82b08125d336d9229af344584782065aec95537e
Filipe Melo <filipexpert63@gmail.com>
2024-12-22 20:09:10 -0300

    Fix 404 in rout

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects', // arquivo de redirecionamento na raiz
+          dest: '.',         // será copiado para a raiz de `dist`
+        },
+      ],
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
   },
 });
